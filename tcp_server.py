ec5847ff813711911b7150fc8f3ebb7f13044447
RanaJT <127953290+RanaJT@users.noreply.github.com>
2023-05-25 15:18:32 +0100

    Update tcp_server.py

--- tcp_server.py
+++ tcp_server.py
@@ -13,20 +13,30 @@ server_socket.bind(server_address) # socket addressing
 server_socket.listen(1)
 
 print('Server is listening on', server_address)
+  try:
+        # Receive data from the client
+        data = client_socket.recv(1024)
+        decoded_data = data.decode()
+        print('Received:', decoded_data)
 
-while True:
-    # Wait for a client to connect
-    client_socket, client_address = server_socket.accept()
-    print('Client connected:', client_address)
+        # Send a response back to the client
+        response = f'Your data has been recieved. Welcome, {decoded_data}.'
+        client_socket.sendall(response.encode())
+   
+    except Exception as e:
+        print("Error occured while processing client request: ", e)
+    
+    finally:
+        # Close the client socket
+        client_socket.close()
 
-    # Receive data from the client
-    data = client_socket.recv(1024)
-    decoded_data = data.decode()
-    print('Received:', decoded_data)
+except KeyboardInterrupt:
+   #Handle Ctrl+C request
+   print("Server inturrupted.")
 
-    # Send a response back to the client
-    response = f'Your data has been recieved. Welcome, {decoded_data}.'
-    client_socket.sendall(response.encode())
+except Exception as e:
+   print("An error occured: ", e)
 
-    # Close the client socket
-    client_socket.close()
+finally:
+   #close the server socket
+   server_socket.close()
