33593539fe03c1f61a36d87e21f0b53526c55ace
RanaJT <127953290+RanaJT@users.noreply.github.com>
2023-05-25 15:43:40 +0100

    Update tcp_client.py
    exception handling added

--- tcp_client.py
+++ tcp_client.py
@@ -1,20 +1,34 @@
 import socket
 from socket import AF_INET, SOCK_STREAM
 
-# Create a TCP/IP socket(AF = Address Family)
-client_socket = socket.socket(AF_INET, SOCK_STREAM)
+try:
+    # Create a TCP/IP socket(AF = Address Family)
+    client_socket = socket.socket(AF_INET, SOCK_STREAM)
+    try:
+        # 1.) Connect to the server (localhost = 127.0.0.1) 
+        server_address = ('localhost', 8000)
+        client_socket.connect(server_address)
+        try:
+            # 2.) Send message over to the TCP-Server
+            message = 'Liano'
+            client_socket.sendall(message.encode())
 
-# 1.) Connect to the server (localhost = 127.0.0.1) 
-server_address = ('localhost', 8000)
-client_socket.connect(server_address)
+            # 3.) Receive Response from TCP-Server
+            response = client_socket.recv(1024)
+            print('Received:', response.decode())
+      
+      except Exception as e:
+            print("Error occurred while transmitting data: ", e)
+   
+  except ConnectionRefusedError:
+            print("Connention refused. Make sure the server is running")
 
-# 2.) Send message over to the TCP-Server
-message = 'Liano'
-client_socket.sendall(message.encode())
+    except Exception as e:
+            print("An error occurred during connection")
+   
+  finally:
+            # Closing the client socket
+            client_socket.close()
 
-# 3.) Receive Response from TCP-Server
-response = client_socket.recv(1024)
-print('Received:', response.decode())
-
-# Closing the client socket
-client_socket.close()
+        except Exception as e:
+      print("An error occured: ", e)
